0cf6f92c8ae677f0ad56eb75438f05d41316f875
MaLeK Darwish <malekksa505@gmail.com>
2024-05-30 09:10:08 +0300

    interface

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import UserProvider from "@/providers/UserProvider";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <body className={inter.className}>
+        <Toaster position="top-center" />
+        <UserProvider>{children}</UserProvider>
+      </body>
     </html>
   );
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,112 +1,130 @@
-import Image from "next/image";
+"use client";
+import { useUserContext } from "@/context/userContext";
+import useRedirect from "@/hooks/useUserRedirect";
+import { useState } from "react";
+import ChangePasswordForm from "./Components/auth/ChangePasswordForm/ChangePasswordForm";
 
 export default function Home() {
+  useRedirect("/login");
+  const {
+    logoutUser,
+    user,
+    handlerUserInput,
+    userState,
+    updateUser,
+    emailVerification,
+    allUsers,
+    deleteUser,
+  } = useUserContext();
+  const { name, photo, isVerified, bio } = user;
+
+  // state
+  const [isOpen, setIsOpen] = useState(false);
+
+  // function
+  const myToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="py-[2rem] mx-[10rem]">
+      <header className="flex justify-between">
+        <h1 className="text-[2rem] font-bold">
+          Welcome <span className="text-red-600">{name}</span>
+        </h1>
+        <div className="flex items-center gap-4">
+          <img
+            src={photo}
+            alt={name}
+            className="w-[40px] h-[40px] rounded-full"
+          />
+          {!isVerified && (
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+              onClick={emailVerification}
+            >
+              Verify Account
+            </button>
+          )}
+
+          <button
+            onClick={logoutUser}
+            className="px-4 py-2 bg-red-600 text-white rounded-md"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Logout
+          </button>
         </div>
-      </div>
+      </header>
+      <section>
+        <p className="text-[#999] text-[2rem]">{bio}</p>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <h1>
+          <button
+            onClick={myToggle}
+            className="px-4 py-2 bg-[#2ECC71] text-white rounded-md"
+          >
+            Update Bio
+          </button>
+        </h1>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {isOpen && (
+          <form className="mt-4 px-8 py-4 max-w-[520px] w-full rounded-md">
+            <div className="flex flex-col">
+              <label htmlFor="bio" className="mb-1 text-[#999]">
+                Bio
+              </label>
+              <textarea
+                name="bio"
+                defaultValue={bio}
+                className="px-4 py-3 border-[2px] rounded-md outline-[#2ECC71] text-gray-800"
+                onChange={(e) => handlerUserInput("bio")(e)}
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              onClick={(e) => updateUser(e, { bio: userState.bio })}
+              className="mt-4 px-4 py-2  bg-blue-500 text-white rounded-md"
+            >
+              Update Bio
+            </button>
+          </form>
+        )}
+      </section>
+      <div className="mt-4 flex gap-8">
+        <div className="flex-1">
+          <ChangePasswordForm />
+        </div>
+        <div className="flex-1">
+          {user.role === "admin" && (
+            <ul>
+              {allUsers.map(
+                (user: any, i: number) =>
+                  user.role !== "admin" && (
+                    <li
+                      key={i}
+                      className="mb-2 px-2 py-3 border grid grid-cols-4 items-center gap-8 rounded-md"
+                    >
+                      <img
+                        src={user.photo}
+                        alt={user.name}
+                        className="w-[40px]  h-[40px] rounded-full"
+                      />
+                      <p>{user.name}</p>
+                      <p>{user.bio}</p>
+                      <button
+                        className="bg-red-500 text-white p-2 rounded-md"
+                        onClick={() => {
+                          deleteUser(user._id);
+                        }}
+                      >
+                        Delete User
+                      </button>
+                    </li>
+                  )
+              )}
+            </ul>
+          )}
+        </div>
       </div>
     </main>
   );
